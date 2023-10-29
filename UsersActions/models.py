d1c3ed6a345d143de6654202abcb186be49dc788
essaon <carlionminon@gmail.com>
2023-10-29 15:05:36 +0300

    None

--- UsersActions/models.py
+++ UsersActions/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+
+class Questions(models.Model):
+    question_text = models.CharField(max_length=200)
+    publish_date = models.DateField('date published')
+
+    def __str__(self):
+        return self.question_text
+
+
+class Choise(models.Model):
+    question_text = models.ForeignKey(Questions, on_delete=models.CASCADE)
+    choice_text = models.CharField(max_length=100)
+    votes = models.IntegerField(default=0)
+
+    def __str__(self):
+        return self.choice_text
