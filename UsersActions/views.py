32dea08b935fdf940bbbedc25642a91dff1ecdfc
essaon <carlionminon@gmail.com>
2023-10-29 18:20:27 +0300

    init

--- UsersActions/views.py
+++ UsersActions/views.py
@@ -1,4 +1,7 @@
+from django.http import Http404
 from django.shortcuts import render
+
+from sity.UsersActions.admin import QuestionAdmin
 from . import models
 
 
@@ -7,3 +10,11 @@ def index(request):
     latest_question = models.Questions.objects.order_by('-publish_date')[:5]
     context = {'latest_question': latest_question}
     return render(request, 'home.html', context)
+
+
+def details(request, question_id):
+    try:
+        question = models.Questions.objects.get(pk=question_id)
+    except models.Questions.DoesNotExist:
+        raise Http404("Question does not exist")
+    return render(request, 'result.html', {'question': question})
